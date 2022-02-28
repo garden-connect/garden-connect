cd7d45c505f94f25328a799e670ea1c27934b0c9
SheamusO <sohara4@cnm.edu>
2022-02-28 16:10:39 -0700

    added navbar

---
+++ frontend/src/ui/shared/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import React from "react"
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import NavLink from "react-bootstrap/NavLink";
+import {Link} from "react-router-dom";
+
+export const NavBar = () => (
+    <header>
+        <Navbar bg="primary" expand="sm" variant="dark">
+            <Link  to="/"><Navbar.Brand>Infinity ÷ -0</Navbar.Brand> </Link>
+                <NavLink>
+                    <Link exact to="/my-profile">My Profile</Link>
+                </NavLink>
+                <NavLink>
+                    <Link exact to="/harvest">
+                        Harvest
+                    </Link>
+                </NavLink>
+                <NavLink>
+                    <Link exact to="/hands">
+                        Hands
+                    </Link>
+                </NavLink>
+                <NavLink>
+                    <Link exact to="/new-post">
+                        New Post
+                    </Link>
+                </NavLink>
+        </Navbar>
+    </header>
+);
