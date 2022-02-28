57c9957d55b33b31859fcce2502e79a4e9a118dd
Alberto Lopez <alopez527@cnm.edu>
2022-02-28 11:38:13 -0600

    finished route and controller for ratings

--- backend/src/apis/rating/rating.controller.ts
+++ backend/src/apis/rating/rating.controller.ts
@@ -61,40 +61,34 @@ export async function postRating(request: Request, response: Response) : Promise
 export async function putRatingController(request: Request, response: Response) : Promise<Response>{
     try {
         const {ratingReviewedProfileId} = request.params
-        const {ratingAmount, ratingContent, ratingDate} = request.body
+        const {ratingReviewingProfileId, ratingAmount, ratingContent} = request.body
         const profile : Profile = request.session.profile as Profile
-        const ratingReviewingProfileId : string = <string>profile.profileId
-
+        const ratingReviewingProfileIdFromSession: string = <string>profile.profileId
+        const rating: Rating = {
+            ratingReviewedProfileId,
+            ratingReviewingProfileId,
+            ratingAmount,
+            ratingContent,
+            ratingDate: null
+        }
         const performRatingUpdate = async (rating: Rating) : Promise<Response> => {
-            const previousRating: Rating = await selectRatingByBothProfileIds(<string>rating.ratingReviewedProfileId) as Rating
+            const previousRating: Rating = await selectRatingByBothProfileIds(rating) as Rating
             const newRating: Rating = {...previousRating, ...rating}
             await updateRating(newRating)
             return response.json({status: 200, data: null, message: "Rating successfully updated"})
         }
 
-        const updatePostFailed = (message: string) : Response => {
+        const updateRatingFailed = (message: string) : Response => {
             return response.json({status: 400, data: null, message})
         }
 
-        return postProfileId === postProfileIdFromSession
-            ? await performPostUpdate({postId, postProfileId, postActive, postCategory, postContent, postDate, postPicture})
-            : updatePostFailed("you are not allowed to perform this action")
+        return ratingReviewingProfileId === ratingReviewingProfileIdFromSession
+            ? await performRatingUpdate(rating)
+            : updateRatingFailed("you are not allowed to perform this action")
     } catch (error: any) {
         return response.json( {status:400, data: null, message: error.message})
     }
 }
 
-export async function getPostByPostCategoryController(request : Request, response: Response) : Promise<Response<Status>>{
-    try {
-        const     {postCategory} = request.params
-        const data  = await selectAllPostsByPostCategory(postCategory)
-        return response.json({status:200, message: null, data});
-    } catch(error) {
-        return response.json({
-            status: 500,
-            message: "current error",
-            data: null
-        })
-    }
-}
+
 
--- backend/src/apis/rating/rating.route.ts
+++ backend/src/apis/rating/rating.route.ts
@@ -1,16 +1,12 @@
 
 import { Router } from 'express';
 import {
-    getAllPostsController,
-    getPostByPostIdController,
-    getPostsByPostProfileIdController,
-    postPost,
-    putPostController,
-    getPostByPostCategoryController,
-    deletePost
-} from './post.controller';
+    getRatingsByReviewedProfileIdController,
+    postRating,
+    putRatingController
+} from "./rating.controller";
 import { asyncValidatorController } from '../../utils/controllers/asyncValidator.controller';
-import { postValidator, categoryValidatorController } from './post.validator';
+import { ratingValidator} from './rating.validator';
 import {isLoggedIn} from "../../utils/controllers/isLoggedIn.controller";
 import {check} from 'express-validator';
 import {checkSchema} from 'express-validator';
@@ -19,36 +15,11 @@ import {checkSchema} from 'express-validator';
 
 const router = Router();
 
-router.route("/:postId")
+router.route("/:ratingReviewedProfileId")
     .get(  asyncValidatorController([
         check("postId", "please provide a valid postId").isUUID()
-    ]), getPostByPostIdController)
-    .put(isLoggedIn, asyncValidatorController(checkSchema(postValidator)), putPostController)
-    .delete(isLoggedIn, asyncValidatorController([
-        check("postId", "please provide a valid postId").isUUID()
-    ]), deletePost);
-
-
-router.route("/postProfileId/:postProfileId")
-    .get(  asyncValidatorController([
-        check("postProfileId", "please provide a valid postProfileId").isUUID()
-    ]), getPostsByPostProfileIdController)
-
-// Every new route is instantiated below. It will include the controller name and the type of action (get, post, delete, put, patch)
-router.route('/')
-    .get( getAllPostsController)
-    .post(isLoggedIn, asyncValidatorController(checkSchema(postValidator)), postPost)
-
-
-//check scheme and validator?
-router.route("/postCategory/:postCategory")
-    .get(
-        asyncValidatorController( [
-            check("postCategory", "please provide a valid category").custom(categoryValidatorController)
-        ])
-        , getPostByPostCategoryController
-    )
-
-
+    ]), getRatingsByReviewedProfileIdController)
+    .post(isLoggedIn, asyncValidatorController(checkSchema(ratingValidator)), postRating)
+    .put(isLoggedIn, asyncValidatorController(checkSchema(ratingValidator)), putRatingController)
 
 export default router;
