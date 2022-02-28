3bc11112f62c1280292597b39e917d1e3d2cd5bb
SheamusO <sohara4@cnm.edu>
2022-02-28 16:45:38 -0700

    startted harvest static page

---
+++ frontend/src/ui/Harvest.js
@@ -0,0 +1,44 @@
+
+
+
+export const Harvest = () => {
+    return(
+        <>
+            <main>
+                <Container>
+                    <Row>
+                    {/*Individual post*/}
+                        <Col>
+                            Profile Id, & date
+                            Image
+                            Post title
+                            Post text
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        {/*Individual post*/}
+                        <Col>
+                            Profile Id, & date
+                            Image
+                            Post title
+                            Post text
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        {/*Individual post*/}
+                        <Col>
+                            Profile Id, & date
+                            Image
+                            Post title
+                            Post text
+                        </Col>
+                    </Row>
+                </Container>
+            </main>
+        </>
+    )
+};
