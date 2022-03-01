050df63385ae488d0ea8fc2b9a8f036fc91bb8f5
SheamusO <sohara4@cnm.edu>
2022-03-01 14:15:03 -0700

    added hands page

---
+++ frontend/src/ui/Hands.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Container, Row, Col} from "react-bootstrap";
+
+
+export const Hands = () => {
+    return(
+        <>
+            <main>
+
+                <Container>
+                    {/*Individual post*/}
+                    <Row>
+                        <Col>
+                            {/*Content sits horizontally in desktop and stacks in mobile*/}
+                            Profile Id and # of stars (Links to profile and ratings)
+                        </Col>
+
+                        <Col>
+                            Message button
+                            {/*Opens dialogue with profile of post*/}
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            Image
+                        </Col>
+                        <Col>
+                            Post title
+                            Post text
+                        </Col>
+                    </Row>
+                </Container>
+                {/*Repeat Container for more posts*/}
+                <Container/>
+                <Container/>
+                <Container/>
+            </main>
+        </>
+    )
+};
