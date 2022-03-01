773d7310f4336dcf6820bcf3bcec45fad223a9fc
Alberto Lopez <alopez527@cnm.edu>
2022-03-01 14:23:49 -0600

    pushing before i pull taylors changes on develop

---
+++ frontend/src/ui/Post.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Container, Row, Col} from "react-bootstrap";
+
+export const Post = () => {
+    return (
+        <>
+            <main>
+                <Container>
+                    <Row>
+                        <Col>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </main>
+        </>
+    )
+}
