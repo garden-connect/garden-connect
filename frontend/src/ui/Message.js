3d8773760c2a57fc601f100b589ef2f62695a71d
Alberto Lopez <alopez527@cnm.edu>
2022-03-02 11:44:01 -0600

    messaging static pages

---
+++ frontend/src/ui/Message.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Container, Row, Col} from "react-bootstrap";
+
+export const Message = () => {
+    return (
+        <>
+            <main>
+                <Container>
+                    <Row>
+                        <Col>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        </Col>
+                    </Row>
+                </Container>
+            </main>
+        </>
+    )
+}
