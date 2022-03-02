0bf7774e1e47ad6d3c7c0115498a6709845e7523
SheamusO <sohara4@cnm.edu>
2022-03-02 13:15:17 -0700

    updated navbar

--- frontend/src/ui/Hands.js
+++ frontend/src/ui/Hands.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Image, Button} from "react-bootstrap";
 
 
 export const Hands = () => {
@@ -7,34 +7,35 @@ export const Hands = () => {
         <>
             <main>
 
-                <Container>
+                <Container className={"border py-5"}>
                     {/*Individual post*/}
                     <Row>
+                        <div>
                         <Col>
                             {/*Content sits horizontally in desktop and stacks in mobile*/}
-                            Profile Id and # of stars (Links to profile and ratings)
+                            <h5>Sheamus #stars</h5>
                         </Col>
 
-                        <Col>
-                            Message button
+                        <Col align-right>
+
+                            <Button>Message</Button>
                             {/*Opens dialogue with profile of post*/}
                         </Col>
+                        </div>
                     </Row>
 
                     <Row>
-                        <Col>
-                            Image
+                        <Col className={"align-top"}>
+                            <Image className={"d-block mx-auto"} src="https://via.placeholder.com/200"></Image>
                         </Col>
                         <Col>
-                            Post title
-                            Post text
+                            <h3>Looking to get some help</h3>
+                            <p>Post text</p>
                         </Col>
                     </Row>
                 </Container>
                 {/*Repeat Container for more posts*/}
-                <Container/>
-                <Container/>
-                <Container/>
+
             </main>
         </>
     )
--- frontend/src/ui/shared/components/NavBar.js
+++ frontend/src/ui/shared/components/NavBar.js
@@ -2,29 +2,58 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import NavLink from "react-bootstrap/NavLink";
 import {Link} from "react-router-dom";
+import {Nav, NavDropdown, Container} from "react-bootstrap";
 
 export const NavBar = () => (
-    <header>
-        <Navbar bg="primary" color="white" expand="sm" variant="dark">
-            <Link  to="/"><Navbar.Brand>Garden Connect</Navbar.Brand> </Link>
-                <NavLink>
+    // <header>
+    //     <Navbar className={"light" } expand="lg" >
+    //         <Link  to="/"><Navbar.Brand>Garden Connect</Navbar.Brand> </Link>
+    //             <NavLink>
+    //                 <Link exact to="/">My Profile</Link>
+    //             </NavLink>
+    //             <NavLink color={"success"}>
+    //                 <Link exact to="/harvest">
+    //                     Harvest
+    //                 </Link>
+    //             </NavLink>
+    //             <NavLink>
+    //                 <Link exact to="/hands">
+    //                     Hands
+    //                 </Link>
+    //             </NavLink>
+    //             <NavLink>
+    //                 <Link exact to="/post">
+    //                     Create Post
+    //                 </Link>
+    //             </NavLink>
+    //         <NavLink>
+    //             <Link exact to="/signup">
+    //                 Sign-up
+    //             </Link>
+    //         </NavLink>
+    //     </Navbar>
+    // </header>
+<Navbar bg="light" expand="lg">
+    <Container>
+        <Navbar.Brand href="#home">Garden Connect</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">
                     <Link exact to="/">My Profile</Link>
-                </NavLink>
-                <NavLink>
-                    <Link exact to="/harvest">
-                        Harvest
-                    </Link>
-                </NavLink>
-                <NavLink>
-                    <Link exact to="/hands">
-                        Hands
-                    </Link>
-                </NavLink>
-                <NavLink>
-                    <Link exact to="/new-post">
-                        New Post
-                    </Link>
-                </NavLink>
-        </Navbar>
-    </header>
+                </Nav.Link>
+                <Nav.Link href="#link"> <Link exact to="/harvest">
+                    Harvest
+                </Link>
+                </Nav.Link>
+                <Nav.Link href="#home">
+                    <Link exact to="/hands">Hands</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                    <Link exact to="/post">New Post</Link>
+                </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Container>
+</Navbar>
 );
