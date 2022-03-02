b4e9f3087eff17192ab1308c86ff4c65071fa595
Taylor Badraun <tbadraun@cnm.edu>
2022-03-02 14:58:21 -0600

    changes to profile, added buttons and tabs

--- frontend/src/ui/Profile.js
+++ frontend/src/ui/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Button, Tabs, Tab} from "react-bootstrap";
 
 export const Profile = () => {
     return (
@@ -9,31 +9,43 @@ export const Profile = () => {
                     {/*ProfileId Rating/Review Header*/}
                     <Row>
                         <Col>
-                            ProfileId, # of Stars, # of Reviews (link to reviews page)
+                            ProfileName *****<a>(12)</a>
+                        {/*click on stars or number to go to rating/review page. (12) means 12 reviews written*/}
                         </Col>
                         {/*Edit Profile Button*/}
                         <Col>
-                            Edit Profile Button on the right side (Won't be visible when viewing other profiles)
+                            <Button variant={"secondary"}>Edit Profile</Button>
+                             {/*(Won't be visible when viewing other profiles)*/}
                         </Col>
                     </Row>
                     {/*About Me*/}
                     <Row>
                         <Col>
-                            A quick blurb about me. 1000 characters
+                            A quick blurb about me. 1000 characters. blaha blah blah blah
                         </Col>
                     </Row>
                     {/*Posts Section*/}
                     <Row>
-                        <Col>
-                            2 Tabs: Active Posts & Previous Posts
-                            Message History Button to the right (goes to message modal)
+                        <Col sm={6}>
+                            <Tabs defaultActiveKey="Active Posts" id="tabs" className="mb-3">
+                                <Tab eventKey="Active Posts" title="Active Posts">
+                                    <p>Post</p>
+                                </Tab>
+                                <Tab eventKey="Previous Posts" title="Previous Posts">
+                                    <p>Old Posts</p>
+                                </Tab>
+                            </Tabs>
+                            {/*2 Tabs: Active Posts & Previous Posts*/}
+                            {/*Message History Button to the right (goes to message modal)*/}
+                        </Col>
+                        <Col sm={6}>
+                            <Button>Message History</Button>
                         </Col>
                     </Row>
                     {/*Post Tab Content*/}
                     <Row>
                         <Col>
-                            Scroll through Post content
-                            Each post has Message Icon if viewing other peoples' profiles or Edit Icon if viewing own profile page
+                            {/*Scroll through Post content. Each post has Message Icon if viewing other peoples' profiles or Edit Icon if viewing own profile page*/}
                         </Col>
                     </Row>
                 </Container>
