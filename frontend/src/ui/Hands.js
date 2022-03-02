eb79f991b4f153afcdfc27e81bf961e52b1a0540
SheamusO <sohara4@cnm.edu>
2022-03-02 13:25:33 -0700

    updated navbar

--- frontend/src/ui/Hands.js
+++ frontend/src/ui/Hands.js
@@ -11,14 +11,11 @@ export const Hands = () => {
                     {/*Individual post*/}
                     <Row>
                         <div>
-                        <Col>
+                        <Col lg={6}>
                             {/*Content sits horizontally in desktop and stacks in mobile*/}
-                            <h5>Sheamus #stars</h5>
-                        </Col>
+                            <h5>Sheamus (#stars)</h5>
 
-                        <Col align-right>
-
-                            <Button>Message</Button>
+                            <Button className={"justify-content-end"}>Message</Button>
                             {/*Opens dialogue with profile of post*/}
                         </Col>
                         </div>
@@ -30,7 +27,7 @@ export const Hands = () => {
                         </Col>
                         <Col>
                             <h3>Looking to get some help</h3>
-                            <p>Post text</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </Col>
                     </Row>
                 </Container>
--- frontend/src/ui/shared/components/NavBar.js
+++ frontend/src/ui/shared/components/NavBar.js
@@ -2,37 +2,9 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import NavLink from "react-bootstrap/NavLink";
 import {Link} from "react-router-dom";
-import {Nav, NavDropdown, Container} from "react-bootstrap";
+import {Nav, Container} from "react-bootstrap";
 
 export const NavBar = () => (
-    // <header>
-    //     <Navbar className={"light" } expand="lg" >
-    //         <Link  to="/"><Navbar.Brand>Garden Connect</Navbar.Brand> </Link>
-    //             <NavLink>
-    //                 <Link exact to="/">My Profile</Link>
-    //             </NavLink>
-    //             <NavLink color={"success"}>
-    //                 <Link exact to="/harvest">
-    //                     Harvest
-    //                 </Link>
-    //             </NavLink>
-    //             <NavLink>
-    //                 <Link exact to="/hands">
-    //                     Hands
-    //                 </Link>
-    //             </NavLink>
-    //             <NavLink>
-    //                 <Link exact to="/post">
-    //                     Create Post
-    //                 </Link>
-    //             </NavLink>
-    //         <NavLink>
-    //             <Link exact to="/signup">
-    //                 Sign-up
-    //             </Link>
-    //         </NavLink>
-    //     </Navbar>
-    // </header>
 <Navbar bg="light" expand="lg">
     <Container>
         <Navbar.Brand href="#home">Garden Connect</Navbar.Brand>
