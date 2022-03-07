093547543855fa839c6a5721f83772b4f61fae60
Kaitlin Schramm <kaitlinschramm@gmail.com>
2022-03-07 09:04:48 -0700

    working on signin and signup

--- frontend/src/ui/Signup.js
+++ frontend/src/ui/Signup.js
@@ -1,14 +1,16 @@
 import React from "react";
-import {Button, Form, Container} from "react-bootstrap";
+import {Button, Form, Container, Row, Col} from "react-bootstrap";
 
 
 export function Signup() {
     return (
         <>
             <Container>
-                <h2>Please fill out the following fields to create an account.</h2>
+                <h2>Welcome to Garden Connect!<br/> This site is meant to build community and facilitate interaction between gardeners and homesteaders in Albuquerque, New Mexico. Take a look around to see what gardeners near you are up to; sign up to become part of the community! </h2>
+                <p className={"mt-5"}>Please fill out the following fields to create an account</p>
             </Container>
-            <Form>
+           <Container>
+            <Form className="w-50">
                 <Form.Group className="mb-3" controlId="formBasicFirstName">
                     <Form.Label>Username</Form.Label>
                     <Form.Control type="firstName" placeholder="Enter your first name" />
@@ -41,10 +43,30 @@ export function Signup() {
                     <Form.Label>Password Confirmation</Form.Label>
                     <Form.Control type="password" placeholder="Please re-enter your password to confirm" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
             </Form>
+           </Container>
+
+            <Container>
+                <Row>
+                    <Col lg={2} className={"mb-5"}>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    </Col>
+                    <Col lg={4}>
+                    <p>
+                    After creating a profile, click here to sign in!
+                    </p>
+                    </Col>
+                    <Col lg={2}>
+                        <Button href='/signin' variant="primary" type="submit">
+                            Sign in
+                        </Button>
+                    </Col>
+
+                </Row>
+            </Container>
+
 
         </>
     )
