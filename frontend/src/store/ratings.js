0baa6e8802eb06334fea5a9bc658c7f7f3418371
Taylor Badraun <tbadraun@cnm.edu>
2022-03-09 21:28:38 -0600

    added fetch statements to ratings.js

--- frontend/src/store/ratings.js
+++ frontend/src/store/ratings.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { createSlice } from "@reduxjs/toolkit";
 import { httpConfig } from '../utils/http-config'
 
@@ -16,6 +17,12 @@ export const {getRatingsByReviewedProfileId} = slice.actions
 export const fetchRatingsByReviewedProfileId = (id) => async dispatch => {
     const {data} = await httpConfig(`/apis/rating/${id}`);
     dispatch(getRatingsByReviewedProfileId(data))
+}
+export const fetchAllRatersRatings = (id) => async (dispatch, getState) => {
+    const {data} = await httpConfig(`/apis/rating/${id}`);
+    await dispatch(fetchRatingsByReviewedProfileId(data))
+    const reviewingIds = _.uniq(_.map(getState().ratings, "ratingReviewingProfileId"))
+    reviewingIds.forEach(id => dispatch(fetchRatingsByReviewedProfileId(id)));
 }
 
 export default slice.reducer
