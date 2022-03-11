fec1c655462f7e60ee53efcd77034aa96e983499
Taylor Badraun <tbadraun@cnm.edu>
2022-03-11 11:13:29 -0600

    deleted excess code and imports

--- frontend/src/ui/Hands.js
+++ frontend/src/ui/Hands.js
@@ -18,24 +18,6 @@ export const Hands = () => {
 
     const posts = useSelector(state => (state.posts ? state.posts : []));
     const postsActive = posts.filter(post => post.postActive === 1);
-    // const postsInactive = posts.filter(post => post.postActive === 0);
-    // // console.log(posts)
-    // const profile = useSelector(state => (state.profiles ? state.profiles[0] : null));
-    // // // console.log(profile)
-    // const ratings = useSelector(state => (state.ratings ? state.ratings : []));
-    // // // const ratings = useSelector(state => (state.ratings ? state.ratings[0] : null));
-    // // // console.log(ratings)
-    // const ratingsAmount = ratings.map(rating => rating.ratingAmount)
-    // // // console.log(ratingsAmount)
-    // const ratingsNumber = ratingsAmount.map(x => parseInt(x, 10))
-    // // // console.log(ratingsNumber)
-    // const ratingsAverage = ratingsNumber.reduce((a,b) => a + b, 0)/ratingsNumber.length;
-    // const profiles = useSelector(state => state.profiles ? state.profiles : null)
-    // // console.log(profiles)
-    // const FindProfileName = () => {
-    //     // const profile = profiles.find(profile => post.postProfileId === profile.profileId)
-    //     // console.log(profile)
-    // }
 
     return (
         <>
