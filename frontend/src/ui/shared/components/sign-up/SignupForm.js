24d92cf19497b0003f70cf859c4e5b7fdaaf19f4
Kaitlin Schramm <kaitlinschramm@gmail.com>
2022-03-10 17:27:48 -0700

    did mailgun workaround

--- frontend/src/ui/shared/components/sign-up/SignupForm.js
+++ frontend/src/ui/shared/components/sign-up/SignupForm.js
@@ -8,28 +8,25 @@ import {SignupFormContent} from "./SignupFormContent";
 export const SignupForm = () => {
     const signUp = {
         profileEmail: "",
-        profileAtHandle: "",
+        profileName: "",
         profilePassword: "",
         profilePasswordConfirm: "",
-        profilePhone: "",
-        profileAvatar: "",
+
     };
 
     const validator = Yup.object().shape({
         profileEmail: Yup.string()
             .email("email must be a valid email")
             .required('email is required'),
-        profileAtHandle: Yup.string()
-            .required("profile handle is required"),
+        profileName: Yup.string()
+            .required("username is required"),
         profilePassword: Yup.string()
             .required("Password is required")
             .min(8, "Password must be at least eight characters"),
         profilePasswordConfirm: Yup.string()
             .required("Password Confirm is required")
             .min(8, "Password must be at least eight characters"),
-        profilePhone: Yup.string()
-            .min(10, "phone number is to short")
-            .max(10, "phone Number is to long")
+
     });
 
     const submitSignUp = (values, {resetForm, setStatus}) => {
