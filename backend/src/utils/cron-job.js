4c0621fd5b1851dabb8cb239ad242ff693775b3a
Alberto Lopez <alopez527@cnm.edu>
2022-03-15 16:02:12 -0500

    trying out cron

--- backend/src/utils/cron-job.js
+++ backend/src/utils/cron-job.js
@@ -1,24 +1,9 @@
-// const cronSeconds = require('node-cron');
-//
-// cronSeconds.schedule('* * 11 15 mar tues', () => {
-//     console.log('running cronSeconds every second');
-// });
-
-//Test
 const cron = require('node-cron');
 
-cron.schedule('* 12 15 mar tues', () => {
+cron.schedule('* * 14 15 March Tuesday', () => {
     console.log('running a task every minute');
 });
 
 
 
 
-
-//after 30 days, execute
-
-// const Cron = require('node-cron');
-//
-// Cron.schedule('59 0 30 * *', () => {
-//     console.log('running a task every minute');
-// });
