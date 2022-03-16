c781fbe468526a3f75a5ace976241f645bf705b2
Alberto Lopez <alopez527@cnm.edu>
2022-03-16 11:56:39 -0500

    changed the more or less button to be arrows

--- backend/src/utils/cron-job.js
+++ backend/src/utils/cron-job.js
@@ -1,7 +1,11 @@
+import {getAllPosts} from "frontend/src/store/posts"
+import {updatePost} from "./post/updatePost";
+
 const cron = require('node-cron');
 
 cron.schedule('1 1 0 1 jan-dec mon', () => {
-    console.log('running a task every minute');
+    getAllPosts(),
+    updatePost()
 });
 
 
