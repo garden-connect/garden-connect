8d37a2a20586a56efd52170fe5069a73e8b906da
Taylor Badraun <tbadraun@cnm.edu>
2022-03-16 13:45:53 -0500

    changed conversation fetch request to include profiles of people conversed with

--- frontend/src/store/conversations.js
+++ frontend/src/store/conversations.js
@@ -3,6 +3,7 @@ import {createSlice} from "@reduxjs/toolkit";
 import {httpConfig} from "../utils/http-config";
 import {act} from "react-dom/test-utils";
 import {fetchPostByPostId} from "./posts";
+import {fetchProfileByProfileId} from "./profiles";
 
 
 
@@ -34,9 +35,12 @@ const slice = createSlice({
 export const {getConversationsContainingProfileId, getConversationsByPostId} = slice.actions
 
 
-export const fetchConversationsContainingProfileId = (id) => async dispatch => {
+export const fetchConversationsContainingProfileId = (id) => async (dispatch, getState) => {
     const {data} = await httpConfig(`/apis/conversation/conversationProfileId/${id}`);
-    dispatch(getConversationsContainingProfileId(data))
+    await dispatch(getConversationsContainingProfileId(data))
+    const conversationIds = _.uniq(_.map(getState().conversations, "conversationSendProfileId"))
+    conversationIds.forEach(id=> dispatch(fetchProfileByProfileId(id)))
+
 }
 
 export const fetchConversationsAndPosts = (id) => async (dispatch, getState) => {
