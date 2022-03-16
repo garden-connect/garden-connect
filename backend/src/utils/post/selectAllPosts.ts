e04fbb8f29c6f0330415cb2f8c6d8af58d658e7e
Kaitlin Schramm <kaitlinschramm@gmail.com>
2022-03-15 18:23:43 -0600

    fixed weird symbols in the backend

--- backend/src/utils/post/selectAllPosts.ts
+++ backend/src/utils/post/selectAllPosts.ts
@@ -4,13 +4,23 @@ import {Profile} from "../interfaces/Profile";
 import {Status} from "../interfaces/Status";
 import {RowDataPacket,} from "mysql2"
 
+const validator = require("validator")
+
 export async function selectAllPosts() : Promise<Post[]> {
     try {
         const mySqlConnection = await connect()
         const mySqlQuery = 'SELECT BIN_TO_UUID(postId) AS postId, BIN_TO_UUID (postProfileId) AS postProfileId, postActive, postCategory, postContent, postDate, postPicture, postTitle FROM post ORDER BY postDate DESC'
         const result = await mySqlConnection.execute(mySqlQuery) as RowDataPacket[]
         await mySqlConnection.release()
-        return result[0] as Array<Post>
+        const posts: Post[] = []
+        for (const row of result[0] as RowDataPacket[]){
+            const {postId, postProfileId, postActive, postCategory, postContent, postDate, postPicture, postTitle} = row
+            posts.push({postId, postProfileId, postActive, postCategory, postContent:validator.unescape(postContent), postDate, postPicture, postTitle:validator.unescape(postTitle)})
+            console.log(validator.unescape)
+        }
+        return(posts)
+
+
     } catch (error) {
         throw error
     }
