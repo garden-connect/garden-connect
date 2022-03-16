e51eeef6ccfc48c163ed7757caf31b27779a9941
Alberto Lopez <alopez527@cnm.edu>
2022-03-16 16:19:35 -0500

    cant figure out the fetch by date

--- backend/src/utils/cron-job.js
+++ backend/src/utils/cron-job.js
@@ -1,11 +1,13 @@
-import {getAllPosts} from "frontend/src/store/posts"
+import {fetchAllPosts, getAllPosts} from "frontend/src/store/posts"
 import {updatePost} from "./post/updatePost";
+import {Post} from "./interfaces/Post";
+import {connect} from "./database.utils";
+import {ResultSetHeader, RowDataPacket} from "mysql2";
 
 const cron = require('node-cron');
 
-cron.schedule('1 1 0 1 jan-dec mon', () => {
-    getAllPosts(),
-    updatePost()
+cron.schedule('1 1 0 * jan-dec 0-7', () => {
+    console.log("hello")
 });
 
 
